6a2298dd8afbba6c276ca95f02c428e4bec46405
akarshi19 <akarshigmathur@gmail.com>
2025-01-15 14:34:44 +0530

    Initial commit

---
+++ src/homeComponents/footer.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+//import GitHubButton from 'react-github-btn'
+import "./style.css";
+class Footer extends Component {
+    render() {
+        return (
+            <footer class="page-footer font-small special-color-dark pt-4 ">
+
+                <div class="footer-copyright text-center py-3">© 2024 Copyright:
+                    <a href="https://github.com/akarshi19" style={{color: "#65b6f0"}}> Akarshi Mathur</a>
+                    
+                </div>
+
+            </footer>
+        );
+    }
+}
+
+export default Footer;
